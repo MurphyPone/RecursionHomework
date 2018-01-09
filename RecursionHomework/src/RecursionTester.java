a891705371bb60bf8e74b098a0054b5deb6268c7
"[MurphyPOne]" <"[murphyp1@bishopireton.org]">
2018-01-08 19:49:16 -0500

    Recovered after deleting >:(

--- RecursionHomework/src/RecursionTester.java
+++ RecursionHomework/src/RecursionTester.java
@@ -1,14 +1,19 @@
 
 public class RecursionTester {
 	public static void main(String[] args) {
-		int testNum = 4;
-		int bigNum = 55555;
+		int testNum = 5;
+		int bigNum = 55055;
 		
 		System.out.println(testNum +"! = " + factorial(testNum));
-		System.out.println(testNum +"st/nd/rd/th digit of the fibonacci sequence = " + fib(testNum));
-		System.out.println("Sum of the digits of " + bigNum +  " = " + fib(bigNum));
+		System.out.println(testNum + "th digit of the Fibonacci sequence = " + fib(testNum));
+		System.out.println("Sum of the digits of " + bigNum +  " = " + sumOfDigits(bigNum));
+		System.out.println("# of 5s in " + bigNum +  " = " + count5s(bigNum));
 
+		System.out.printf("Printing the digits from %d down to 0 and then back up\n", testNum);
+		descAsc(testNum);
 	}
+	
+	//Asked Patrick for the prompts 
 	
 	//factorial
 	private static int factorial(int n ) {
@@ -20,7 +25,7 @@ public class RecursionTester {
 	
 	//Fibonacci Sequence
 	private static int fib(int n) {
-		if (n < 2) //base case 
+		if(n < 2)	//Base case
 			return n;
 		else 
 			return fib(n-1) + fib(n-2);	//Googled expression : X(n) = X(n-1) + X(n-2)...
@@ -33,7 +38,29 @@ public class RecursionTester {
 		else 
 			return (n % 10) + sumOfDigits(n/10);	//Add last digit, remove last digit, recurse 
 	}
+	
 	//count5s
+	private static int count5s(int n) {
+		if(n < 10)	//Base cases
+			if(n == 5)	
+				return 1;
+			else 
+				return 0;
+		if(n % 10 == 5)	//Had to google this, I wasn't using modulo, the rest of my code was pretty much the same 
+			return 1 + count5s(n/10);	
+		else 
+			return count5s(n/10);
+	}
+	
 	//DescAsc
+	private static void descAsc(int n) {
+		if(n == 0) 
+			System.out.println(n);
+		else {
+			System.out.println(n);
+			descAsc(n-1);
+			System.out.println(n);
+		}
+	}
 
 }
